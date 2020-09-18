45d5b8f6a39d29efa42f98613a99aa6f6b6e3bc8
MarcelinaWojnarowska <sz.p.marcelina_w@wp.pl>
2020-09-18 21:43:21 +0200

    fix sql

--- app/db.py
+++ app/db.py
@@ -6,14 +6,10 @@ def create_schema():
     connection = sqlite3.connect('my_book.db')
     c = connection.cursor()
 
-    c.executescript(""" DROP TABLE IF EXISTS books; DROP TABLE IF EXISTS comments """)
+    c.executescript(""" DROP TABLE IF EXISTS books; DROP TABLE IF EXISTS comments; 
+    CREATE TABLE IF NOT EXISTS
+    books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE, author TEXT);
+    CREATE TABLE IF NOT EXISTS
+    comments(id INTEGER PRIMARY KEY AUTOINCREMENT, book_id INTEGER, comment TEXT, nickname TEXT)""")
 
-    # create books table
-    command1 = """CREATE TABLE IF NOT EXISTS
-    books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE, author TEXT)"""
-    c.execute(command1)
 
-    # create comments table
-    command2 = """CREATE TABLE IF NOT EXISTS
-    comments(id INTEGER PRIMARY KEY AUTOINCREMENT, book_id INTEGER, comment TEXT, nickname TEXT)"""
-    c.execute(command2)
